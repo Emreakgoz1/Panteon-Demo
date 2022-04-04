6ffb6b1796bb0457df213864da120d765d6ba190
Emre Akgöz <baranemreakgoz@gmail.com>
2022-04-04 16:40:23 +0300

    Initial commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    float Horizontal;
+
+    float Vertical;
+
+    [SerializeField] float MovementForce = 10f;
+
+    float turnSmoothVelocity;
+
+    [SerializeField] float SmoothTurnTime = 0.1f;
+
+    public bool SwitchMove = false;
+
+    Vector3 direction;
+
+    CharacterController cb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cb = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Horizontal = Input.GetAxis("Horizontal");
+        Vertical = Input.GetAxis("Vertical");
+        direction = new Vector3(Horizontal, 0, Vertical);
+        cb.Move(direction * Time.deltaTime * MovementForce);
+
+        
+
+        if (direction.magnitude > 0.01f)
+        {
+            float targetAngle;
+
+            if (!SwitchMove)
+            {
+                targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            }
+            else
+            {
+                targetAngle = Mathf.Atan2(-direction.x,-direction.z) * Mathf.Rad2Deg;
+            }
+
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, SmoothTurnTime);
+            transform.rotation = Quaternion.Euler(0f,angle,0f);
+
+            ////////////////////////animation/////////////////////
+            //// koþu true
+        }
+           /// koþu false
+
+
+    }
+    
+}
