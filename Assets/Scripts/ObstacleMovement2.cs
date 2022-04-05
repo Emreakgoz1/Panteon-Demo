e2ea108a38e06f7c4e6e64fbd17728484d25411f
Emre Akgöz <baranemreakgoz@gmail.com>
2022-04-05 16:13:40 +0300

    Horziontal Obstacle Movement İmproved

--- Assets/Scripts/ObstacleMovement2.cs
+++ Assets/Scripts/ObstacleMovement2.cs
@@ -3,19 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 
-public class ObstacleMovement : MonoBehaviour
+public class ObstacleMovement2 : MonoBehaviour
 {
-    
     // Start is called before the first frame update
     void Start()
     {
         DOTween.Init();
         Sequence Obstacle = DOTween.Sequence();
-        Obstacle.Append(transform.DOMoveX(5, 1));
-        Obstacle.Append(transform.DOMoveX(0, 1));
+        Obstacle.Append(transform.DOMoveX(5,1));
+        Obstacle.Append(transform.DOMoveX(9.6f,1));
         Obstacle.SetLoops(-1);
-
     }
 
-    
 }
