3c5101d4634433dfcdecaee190c10bdd114a8629
Emre Akgöz <baranemreakgoz@gmail.com>
2022-04-07 18:01:32 +0300

    NavMesh Opponents Added

---
+++ Assets/Scripts/OpponentController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class OpponentController : MonoBehaviour
+{
+    [SerializeField] private Transform MovePositionTransform;
+    private NavMeshAgent navMeshAgent;
+    private void Awake()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+    private void Update()
+    {
+        navMeshAgent.destination = MovePositionTransform.position;
+    }
+}
